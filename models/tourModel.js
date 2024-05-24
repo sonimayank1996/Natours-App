0ef27dca531d70308392f46eec0a5977034f23a0
sonimayank0602@gmail.com <sonimayank0602@gmail.com>
2024-05-24 23:12:09 +0530

    document middleware features

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
+
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -7,6 +9,7 @@ const tourSchema = new mongoose.Schema({
       unique: true,
       trim: true
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must be duration']
@@ -52,7 +55,28 @@ const tourSchema = new mongoose.Schema({
       select: false
     },
     startDates: [Date]
+  }, {
+    toJSON:{ virtuals: true },
+    toObject:{ virtuals: true }
+
   });
+
+  tourSchema.virtual('durationWeeks').get(function() {
+    return this.duration / 7;
+  })
+
+  // DOCUMENT MIDDLEWARE: runs before .save() and .create()
+  tourSchema.pre('save', function(next) {
+    console.log(this);
+    this.slug = slugify(this.name, { lower: true})
+    next();
+  });
+
+  // run after the pre document middleware is completed
+  tourSchema.post('save', function(doc, next) {
+    console.log(doc);
+    next();
+  })
 
 const Tour = mongoose.model("Tour", tourSchema);
 
