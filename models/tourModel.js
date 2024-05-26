6a99340ffe02e84ec95f217e42ee4f0953314752
sonimayank0602@gmail.com <sonimayank0602@gmail.com>
2024-05-26 10:21:37 +0530

    aggregation-middleware-features

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,26 +1,26 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
 
-
-const tourSchema = new mongoose.Schema({
+const tourSchema = new mongoose.Schema(
+  {
     name: {
       type: String,
       required: [true, "A tour must have a name"],
       unique: true,
-      trim: true
+      trim: true,
     },
     slug: String,
     duration: {
       type: Number,
-      required: [true, 'A tour must be duration']
+      required: [true, "A tour must be duration"],
     },
     maxGroupSize: {
       type: Number,
-      required: [true, 'A tour must have a group size']
+      required: [true, "A tour must have a group size"],
     },
     difficulty: {
       type: String,
-      required: [true, 'A tour must have a difficulty']
+      required: [true, "A tour must have a difficulty"],
     },
     ratingsAverage: {
       type: Number,
@@ -38,70 +38,76 @@ const tourSchema = new mongoose.Schema({
     summary: {
       type: String,
       trim: true,
-      required: [true, 'A Tour must have a summary']
+      required: [true, "A Tour must have a summary"],
     },
     description: {
       type: String,
-      trim: true
+      trim: true,
     },
     imageCover: {
       type: String,
-      required: [true, 'A tour must hava a cover image']
+      required: [true, "A tour must hava a cover image"],
     },
     images: [String],
     createdAt: {
       type: Date,
       default: Date.now(),
-      select: false
+      select: false,
     },
     startDates: [Date],
     secretTour: {
       type: Boolean,
-      default: false
-    }
-  }, {
-    toJSON:{ virtuals: true },
-    toObject:{ virtuals: true }
+      default: false,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
-  });
+tourSchema.virtual("durationWeeks").get(function () {
+  return this.duration / 7;
+});
 
-  tourSchema.virtual('durationWeeks').get(function() {
-    return this.duration / 7;
-  })
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre("save", function (next) {
+  console.log(this);
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 
-  // DOCUMENT MIDDLEWARE: runs before .save() and .create()
-  tourSchema.pre('save', function(next) {
-    console.log(this);
-    this.slug = slugify(this.name, { lower: true})
-    next();
-  });
+// run after the pre document middleware is completed
+tourSchema.post("save", function (doc, next) {
+  console.log(doc);
+  next();
+});
 
-  // run after the pre document middleware is completed
-  tourSchema.post('save', function(doc, next) {
-    console.log(doc);
-    next();
-  })
+// QUERY MIDDLEWARE
+// this keyword current point to the current query not the document
+// tourSchema.pre('find', function(next) {
+tourSchema.pre("/^find/", function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
 
+tourSchema.post("/^find/", function (doc, next) {
+  console.log(`Query Took ${Date.now() - this.start} milliseconds!`);
+  console.log(doc);
+  next();
+});
 
-  // QUERY MIDDLEWARE
-  // this keyword current point to the current query not the document
-  // tourSchema.pre('find', function(next) {
-  tourSchema.pre('/^find/', function(next) {
-    this.find({ secretTour: { $ne: true }})
-    this.start = Date.now();
-    next();
-  });
+// tourSchema.pre('findOne', function(next) {
+//   this.find({ secretTour: { $ne: true }})
+//   next();
+// })
 
-    tourSchema.post('/^find/', function(doc, next) {
-      console.log(`Query Took ${Date.now() - this.start} milliseconds!`);
-      console.log(doc);
-    next();
-  });
-
-  // tourSchema.pre('findOne', function(next) {
-  //   this.find({ secretTour: { $ne: true }})
-  //   next();
-  // })
+// AGGREGATION MIDDLEWARE
+tourSchema.pre("aggregate", function (next) {
+  this.pipeline.unshift({ $match: { secretTour: { $ne: true } } });
+  next();
+});
 
 const Tour = mongoose.model("Tour", tourSchema);
 
